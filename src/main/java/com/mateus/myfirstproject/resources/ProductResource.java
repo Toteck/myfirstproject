301f01218d2564c2b9fe44f777a7b71c70f3322f
mateuswdo <mateusweslley14@gmail.com>
2022-06-15 16:59:44 -0300

    Product Memory

---
+++ src/main/java/com/mateus/myfirstproject/resources/ProductResource.java
@@ -0,0 +1,40 @@
+package com.mateus.myfirstproject.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mateus.myfirstproject.entities.Product;
+import com.mateus.myfirstproject.repositories.ProductRepository;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+	
+	
+	/*Sempre que você instanciar um objeto ProductResource
+	 * Ele vai obter para você automaticamente uma instância do CatergoryRepository
+	 * Tudo isso debaixo dos panos. Mas para funcionar eu tenho que colocar o @Component (mecanismo de injeção de dependência) 
+	 * no ProductRepository */
+	@Autowired
+	private ProductRepository categoryRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> list = categoryRepository.findAll(); // Vai buscar todos os dados e devolver para lista e depois eu retorno tudo
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product cat = categoryRepository.findById(id);
+		return ResponseEntity.ok().body(cat);
+	}
+}
+
+
